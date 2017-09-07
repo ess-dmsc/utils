09b82b383dd475f748c49807ab8eeef325353a54
Afonso Mukai <afonso.mukai@esss.se>
2017-09-07 11:46:33 +0200

    Remove metrics consumer

--- metricconsumer/getmetrics.py
+++
@@ -1,9 +0,0 @@
-import json
-import pickle
-import requests
-
-
-if __name__ == '__main__':
-    r = requests.get('http://localhost:8080/render?target=kafka.kafka.server.BrokerTopicMetrics.BytesInPerSec.15MinuteRate&format=pickle&from=-5min')
-    data = pickle.loads(r.content)
-    print(data[0])
