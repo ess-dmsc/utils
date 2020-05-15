9f9f13cea55f81ce2ce986e799c64227cd7aaad2
Morten Jagd Christensen <morten.christensen@esss.se>
2020-05-15 12:13:12 +0200

    ome commit

--- versioning/githubrel.py
+++ versioning/githubrel.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import github_release as grel
-import sys, argparse, requests
+import os, sys, argparse, requests, json, re, ast
 
+# replace with 'ess-dmsc/event-formation-unit'
+# or even better infer from the repo in current working dir
 repo = 'mortenjc/sandbox'
 
 # print error message and exit
@@ -10,7 +12,7 @@ def error_exit(errMsg):
   print("Error: {}".format(errMsg))
   sys.exit(1)
 
-
+# check if a release already exists by asking for its info
 def release_exists(tag):
   try:
     grel.get_release_info(repo, tag)
@@ -18,45 +20,75 @@ def release_exists(tag):
     return False
   return True
 
+# check that version tag corresponds to the agreed format, exit if not
+def check_tag_valid(tag):
+    res = re.match('v?([0-9])\.([0-9])\.([0-9])', tag)
+    if res:
+        return True
+    error_exit("tag {} has invalid format".format(tag))
 
+
+# download file from url to file and return filename
+def get_file_from_url(url):
+    filename=os.path.basename(url)
+    if url != "":
+        print("Get asset from URL")
+        try:
+            tmpfile = requests.get(url)
+            open(filename, 'wb').write(tmpmyfile.content)
+            return filename
+        except:
+            error_exit("unable to retrieve url to disk")
+    return ""
+
+
+#def show_releases():
+#    releases = grel.get_releases(repo)
+#    for rel in releases:
+#        res = re.match('tag_name.*:.*v', tag)
 
 #
 # #
 #
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-r', '--release',
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--release',
                        help = "release tag",
                        type = str, required=True, default = "")
-  parser.add_argument('-u', '--url',
+    parser.add_argument('-u', '--url',
                        help = "asset download URL",
-                       type = str, required=True, default = "")
-  parser.add_argument('-c', '--commit_hash',
+                       type = str, default = "")
+    parser.add_argument('-c', '--commit_hash',
                        help = "referenced hash",
-                       type = str, required=True, default = "")
-  parser.add_argument("-d", action='store_true', help = "delete release")
+                       type = str, default = "")
+    parser.add_argument('-d', '--delete', action='store_true', help = "delete release")
+#    parser.add_argument('-l', '--list', action='store_true', help = "list current releases")
 
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if args.d:
-    print("delete release")
-    grel.gh_release_delete(repo, args.release)
-    sys.exit(0)
+    # if args.list:
+    #     show_releases();
+    #     sys.exit(0);
 
-  # Should validate release string
-  print("Check for existing release")
-  if release_exists(args.release):
-    error_exit("Release {} already exists, exiting ...".format(args.release))
+    check_tag_valid(args.release)
 
-  print("Get asset from URL")
-  myfile = requests.get(args.url)
-  open('files.gz', 'wb').write(myfile.content)
+    if args.delete:
+        print("delete release")
+        grel.gh_release_delete(repo, args.release)
+        sys.exit(0)
 
-  print("Creating release")
-  grel.gh_release_create(repo, args.release, publish=True, target_commitish=args.commit_hash, asset_pattern='files.gz')
+    # Should validate release string
+    print("Check for existing release")
+    if release_exists(args.release):
+        error_exit("Release {} already exists, exiting ...".format(args.release))
 
-  print("Done")
+    asset = get_file_from_url(args.url)
+
+    print("Creating release")
+    grel.gh_release_create(repo, args.release, publish=True, target_commitish=args.commit_hash, asset_pattern=asset)
+
+    print("Done")
 
 #
 if __name__ == '__main__':
